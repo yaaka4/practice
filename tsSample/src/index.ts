e60ca007a80c76b195e3b7fcc64d6f82eea7d4fa
yaaka4 <hrk.yszwz@gmail.com>
2022-07-17 22:58:54 +0900

    2-1

--- tsSample/src/index.ts
+++ tsSample/src/index.ts
@@ -1,3 +1,12 @@
 const message: string = "Hello, World2 next";
-
 console.log(message);
+
+const greeting = "Hello, ";
+// コメント
+const text = greeting + "world!";
+console.log(text);
+
+const i: number = 9;
+if (i < 10) {
+    console.log("10未満");
+}
